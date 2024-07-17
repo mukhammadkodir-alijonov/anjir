20a4972519c501240c9836cb9f9f883dbdc59e99
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-18 01:57:32 +0500

    add appdbcontex

--- Anjir.Services.CouponAPI/Data/AppDbContext.cs
+++ Anjir.Services.CouponAPI/Data/AppDbContext.cs
@@ -10,5 +10,17 @@ namespace Anjir.Services.CouponAPI.Data
         }
 
         public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinAmount = 100
+            });
+        }
     }
 }
--- Anjir.Services.CouponAPI/Program.cs
+++ Anjir.Services.CouponAPI/Program.cs
@@ -1,11 +1,12 @@
 using Anjir.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AppDbContext<AppDbContext>(options=>
+builder.Services.AddDbContext<AppDbContext>(options=>
 {
-    options.useSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
