918a75fc45c69304211ec97cebdda1dab231658f
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-17 12:42:28 +0500

    start

---
+++ Anjir.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Anjir.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anjir.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
---
+++ Anjir.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Anjir.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode { get; set; } = string.Empty;
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
